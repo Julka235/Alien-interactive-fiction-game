8284b203a1358b6fb7693b178a10e91be889f618
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2025-11-15 22:25:11 +0100

    added put and stuff for haskell

--- haskell/alien.hs
+++ haskell/alien.hs
@@ -16,7 +16,7 @@ data ThingType
     = Fluff
     | Gun
     | Multitool
-    deriving (Enum, Eq)
+    deriving (Enum, Eq, Show)
 
 -- characters
 data CharacterType
@@ -25,13 +25,13 @@ data CharacterType
     | Becker
     | Walker
     | Reed
-    deriving (Enum, Eq)
+    deriving (Enum, Eq, Show)
 
 -- valid choose
 data ChooseType
     = MedBay
     | Isolation
-    deriving (Enum, Eq)
+    deriving (Enum, Eq, Show)
 
 -- parsing part
 parseRoom :: String -> Maybe RoomType
@@ -76,10 +76,8 @@ describe _ = "There is no such room.\n"
 investigationResponse :: CharacterType -> ChooseType -> String
 
 investigationResponse Lambert _ =
-    "'You were the first here, right, Lambert?'\n"
-    ++ "'Yeah,' she says, voice trembling. 'I was walking down the corridor when the power went out. Then I heard the scream and...' She glances tearfully at Dallas’ body. 'I don’t know who or what could have done this.'\n"
-    ++ "'Did you notice anything else?'\n"
-    ++ "'Not much,' she says, shivering. 'Except for Fluff. The cat ran between my legs just before the scream. He was clearly rattled, yowling and howling.'\n"
+    "'You were the first here, right, Lambert? Did you notice anything?'\n"
+    ++ "'Not much,' she says, voice trembling. 'I was walking down the corridor when the power went out. Then the cat ran between my legs, yowling and howling, just before the scream. And then...' She glances tearfully at Dallas’ body and shivers. 'I don’t know who or what could have done this.'\n"
     ++ "'Fluff’s instincts were always sharp,' you think to yourself. 'If he sensed danger before anyone else... maybe he’s seen what we haven’t.'\n"
 
 investigationResponse Reed _ =
@@ -97,6 +95,7 @@ investigationResponse Walker Isolation =
     ++ "'Did Becker leave quarantine?'\n"
     ++ "'Not exactly,' Walker replies. 'He's still in isolation – but he's dead, Ripley. Blood everywhere, his body torn apart. The strange thing is, no alarm went off, so it wasn't a malfunction. Someone on the crew must have unlocked the door.'\n"
 
+
 -- current world state
 data WorldState = WorldState
   {
@@ -105,6 +104,7 @@ data WorldState = WorldState
   , roomThings         :: [(ThingType, RoomType)]
   , roomCharacters     :: [(CharacterType, RoomType)]
   , deadCharacters     :: [CharacterType]
+  , lights             :: Bool
   , countdown          :: Int
   , hintCounter        :: Int
   , beckerChoice       :: Maybe ChooseType
@@ -133,6 +133,7 @@ initialWorldState = WorldState
       [ Dallas
       , Becker
       ]
+  , lights = True
   , countdown = 3
   , hintCounter = 0
   , beckerChoice = Nothing
@@ -140,6 +141,110 @@ initialWorldState = WorldState
   , investigated = []
   , gameOver = False
   }
+
+gameLoop :: WorldState -> IO ()
+gameLoop ws
+    | gameOver ws = putStrLn "Game Over. Thanks for playing!"
+    | otherwise = do
+        putStr "> "
+        hFlush stdout
+        input <- getLine
+        ws' <- handleCommand input ws
+        gameLoop ws'
+
+handleCommand :: String -> WorldState -> IO WorldState
+handleCommand input ws =
+    case words input of
+        ["Exit"] ->
+            return ws { gameOver = True }
+
+        ["Help"] -> do
+            printHelp
+            return ws
+
+        ["Rooms"] -> do
+            printRooms
+            return ws
+
+        ["Crew"] -> do
+            printCrew
+            return ws
+
+        ["Look"] -> do
+            putStrLn (describe (currentRoom ws))
+            return ws
+
+        ["Go", roomStr] ->
+            handleGo roomStr ws
+
+        ["Take", thingStr] ->
+            handleTake thingStr ws
+
+        ["Investigate", charStr] ->
+            handleInvestigate charStr ws
+
+        ["Choose", choiceStr] ->
+            handleChooseCommand choiceStr ws
+
+        _ -> do
+            putStrLn "Unknown command. Type Help."
+            return ws
+
+applyChoice :: ChooseType -> WorldState -> (String, WorldState)
+applyChoice choice ws
+    | beckerChoice ws /= Nothing =
+        ("You already made your choice. It is too late to change it now.\n", ws)
+    | otherwise =
+        let wsChosen = ws { beckerChoice = Just choice }
+            wsHandled = handleChoiceEffects choice wsChosen
+            (powerMsg, wsFinal) = powerOffScene wsHandled
+            msg = "MU/TH/ER: Your choice has been recorded: " ++ show choice ++ ".. Command sent.\n" ++ powerMsg
+        in (msg, wsFinal)
+
+handleChoiceEffects :: ChooseType -> WorldState -> WorldState
+handleChoiceEffects Isolation ws =
+    ws { hintCounter = hintCounter ws + 1 }
+handleChoiceEffects MedBay ws =
+    ws
+
+powerOffScene :: WorldState -> (String, WorldState)
+powerOffScene ws =
+    let msg = "You stretch and rise from the console, planning to look for the ship's cat, Fluff.\nAs you step into the corridor, the lights go out. The ship is plunged into darkness. The only sound is your own heartbeat, pounding in your ears. Your breath catches when you hear a scream - and stops entirely when it’s cut short.\nYou remember the emergency procedure: in a total blackout, all crew members are to gather in the living quarters."
+        ws' = ws { lights = False }
+    in (msg, ws')
+
+handleChooseCommand :: String -> WorldState -> IO WorldState
+handleChooseCommand arg ws =
+    case parseChoose arg of
+        Nothing -> do
+            putStrLn "You must choose MedBay or Isolation."
+            putStrLn "Try again."
+            return ws
+        Just choice -> do
+            let (msg, ws') = applyChoice choice ws
+            putStrLn msg
+            return ws'
+
+
+handleGo :: String -> WorldState -> IO WorldState
+handleGo roomStr ws =
+    case parseRoom roomStr of
+        Nothing -> do
+            putStrLn "No such room."
+            return ws
+        Just r -> do
+            putStrLn ("You enter the " ++ show r ++ ".")
+            return ws { currentRoom = r }
+
+handleTake :: String -> WorldState -> IO WorldState
+handleTake thingStr ws = do
+    putStrLn "Taking not implemented yet."
+    return ws
+
+handleInvestigate :: String -> WorldState -> IO WorldState
+handleInvestigate charStr ws = do
+    putStrLn "Investigation not implemented yet."
+    return ws
 
 -- help
 printHelp :: IO ()
@@ -153,6 +258,7 @@ printHelp = do
     putStrLn "Rooms                 - to see the list of the rooms."
     putStrLn "Exit                  - to exit the game."
     putStrLn "Help                  - to see this message again."
+    hFlush stdout
     return ()
 
 -- crew
@@ -165,6 +271,7 @@ printCrew = do
     putStrLn "Walker     - chief engineer."
     putStrLn "Becker     - executive officer."
     putStrLn "Reed       - science officer."
+    hFlush stdout
     return ()
 
 -- rooms
@@ -177,22 +284,24 @@ printRooms = do
     putStrLn "TechnicalRoom     - houses the main computer, MU/TH/ER."
     putStrLn "PowerRoom         - controls the ship's entire power system."
     putStrLn "Shuttle           - escape vessel for emergency departure."
+    hFlush stdout
     return ()
 
-start :: IO ()
-start = do
-    putStrLn "Do you want to play a game?'), nl,"
+main :: IO ()
+main = do
+    putStrLn "Do you want to play a game?"
+    _ <- getLine
     putStrLn "You are the Warrant Officer aboard the spaceship Nostromo, on a mission to investigate a newly discovered life form. But something  has gone horribly wrong - and the alien creature may not be the only danger lurking in the ship's dark corridors..."
     putStrLn "But before you continue your journey:"
     printHelp
     putStrLn ""
     putStrLn "MU/TH/ER, main spaceship's computer hums softly when it prints the response on the screen."
-    putStrLn "MU/TH/ER: Hello, Warrant Officer Ripley. Here is the report you requested."
-    putStrLn "Report of Mission 067801"
-    putStrLn "Time 9036727h: Corporate command authorizes the spaceship Nostromo to investigate a possible life form on planet 26-Draconis."
-    putStrLn "Time 9036911h: Nostromo lands on the surface of 26-Draconis. Executive Officer Becker and Science Officer Reed leave the ship to investigate."
-    putStrLn "Time 9036916h: Nostromo loses contact with Executive Officer Becker. Science Officer Reed reports unsuccessful search attempts."
+    putStrLn "MU/TH/ER: Hello, Warrant Officer Ripley. Here is the report of Mission 067801"
+    putStrLn "Corporate command authorized the spaceship Nostromo to investigate a possible life form on planet 26-Draconis. Nostromo landed on the surface of 26-Draconis. Executive Officer Becker and Science Officer Reed left the ship to investigate. After 6 hours, Nostromo lost contact with Executive Officer Becker. Science Officer Reed reported unsuccessful search attempts."
     putStrLn "MU/TH/ER: Anything else I can do for you, Officer?"
     putStrLn "Before you can respond, the main console clears. A new line appears."
     putStrLn "MU/TH/ER: Science Officer Reed has re-entered the Nostromo carrying the sick and unconcious Executive Officer Becker. His spacesuit is breached. Per quarantine law, the crew must be contained. Should I send the command to move him to the medbay for treatment, or to isolation to prevent potential contamination?"
+    putStrLn ""
     putStrLn "Type either 'Choose MedBay' or 'Choose Isolation'."
+    hFlush stdout
+    gameLoop initialWorldState
